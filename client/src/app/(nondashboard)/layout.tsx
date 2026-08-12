acb60e1384a461ae7f3449187fda888f6c6bbb5a
Durgesh <your-github-gaudd221@gmail.com>
2026-08-12 11:33:27 +0530

    fix: add client as regular folder

---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { useGetAuthUserQuery } from "@/state/api";
+import Navbar from "@/components/Navbar";
+import { usePathname, useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+    const pathname = usePathname();
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useEffect(() => {
+      if (authUser) {
+        const userRole = authUser.userRole?.toLowerCase();
+        if (
+          (userRole === "manager" && pathname.startsWith("/search")) ||
+          (userRole === "manager" && pathname === "/")
+        ) {
+          router.push(
+           "/managers/properties",
+            { scroll: false },
+          );
+        }
+      } else {
+        setIsLoading(false);
+      }
+    }, [authUser, router, pathname]);
+  
+    if (authLoading || isLoading) return <>Loading...</>;
+    // if (!authUser?.userRole) return null;
+  
+
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+      <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default layout;
---
+++ client/src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Auth from "./(auth)/authProvider";
+import StoreProvider from "@/state/redux";
+import { Authenticator } from "@aws-amplify/ui-react";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <Authenticator.Provider>
+        <Auth>{children}</Auth>
+      </Authenticator.Provider>
+    </StoreProvider>
+  );
+};
+
+export default Provider;
